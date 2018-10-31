ec9f09de8024137b84ff4414c72dabf57d73fdbe
M. Kaan Tasbas <mktasbas@gmail.com>
2018-10-30 20:03:01 -0400

    commit from linux to move things into windows

--- main.c
+++ main.c
@@ -25,10 +25,13 @@ int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	
+	// set up port 1
 	P1DIR |= BIT2;
 	P1SEL |= BIT2;  // red 1.2
+	// set up port 2
 	P2DIR |= BIT1 + BIT4;
-	P2SEL |= BIT1 + BIT4;   // green 2.1 + blue 2.4
+	P2SEL |= BIT1;  // green 2.1
+	P2OUT |= BIT4;  // blue 2.4
 
 	TA0CCR0 = 1000;
 	TA0CCTL1 = OUTMOD_3;
@@ -37,9 +40,9 @@ int main(void)
 
 	TA1CCR0 = 1000;
 	TA1CCTL1 = OUTMOD_3;
-	TA1CCTL2 = OUTMOD_3;
+	//TA1CCTL2 = OUTMOD_3;
 	TA1CCR1 = 1000;
-	TA1CCR2 = 1000;
+	//TA1CCR2 = 1000;
 	TA1CTL = TASSEL_2 + MC_1;
 
 	// read analog values
@@ -48,23 +51,6 @@ int main(void)
 
 	while(1)
 	{
-//	    setRedPwm(0);
-//	    setGreenPwm(100);
-//	    setBluePwm(100);
-//
-//	    __delay_cycles(500000);
-//
-//	    setRedPwm(100);
-//	    setGreenPwm(0);
-//	    setBluePwm(100);
-//
-//	    __delay_cycles(500000);
-//
-//	    setRedPwm(100);
-//	    setGreenPwm(100);
-//	    setBluePwm(0);
-//
-//	    __delay_cycles(500000);
 
 	    float power = (rand() % 200) + 800;     // generate random number from [0,20], shift to [80,100]
 	    int red_pwm, green_pwm;
@@ -73,14 +59,11 @@ int main(void)
 
 	    // TODO: vary pwm based on random power level. Current calculation isn't working
 
-	    red_pwm = 100 + (50 * power);                  // set pwm to 100 +- 50
-	    green_pwm = 900 + (50 * power);                // set pwm to 900 +- 50
-
-
+	    red_pwm = 100;// + (50 * power);                  // set pwm to 100 +- 50
+	    green_pwm = 900;// + (50 * power);                // set pwm to 900 +- 50
 
 	    setRedPwm(red_pwm);
 	    setGreenPwm(green_pwm);
-	    setBluePwm(1000);
 
 	    __delay_cycles(1000);
 	}
@@ -100,5 +83,5 @@ void setGreenPwm(int pwm)
 
 void setBluePwm(int pwm)
 {
-    TA1CCR2 = pwm;
+    //TA1CCR2 = pwm;
 }
